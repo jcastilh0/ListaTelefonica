ab00c7716e385e9ea1a58b87dfd093633f057a17
Julia Borges Bassi Castilho <juju.ifg2003@gmail.com>
2025-06-22 18:52:34 -0300

    Criada a classe Contato para representar os dados do contato (id, nome, telefone e email)

---
+++ src/main/java/org/projetoIntegrador_IIA/Contato.java
@@ -0,0 +1,54 @@
+package org.projetoIntegrador_IIA;
+
+import java.util.SplittableRandom;
+
+public class Contato {
+    private String idcon;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Contato() {
+        super();
+    }
+
+    public Contato(String idcon, String nome, String email, String telefone) {
+        this.idcon = idcon;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getIdcon() {
+        return idcon;
+    }
+
+    public void setIdcon(String idcon) {
+        this.idcon = idcon;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
